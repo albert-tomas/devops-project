fc5fb0c7f46bb5a236a14ac924c7fa0d08c52331
albert-tomas <alberttomas135@gmail.com>
2026-02-09 20:35:53 +0100

    Add script to generate system users report

--- scripts/users_report.sh
+++ scripts/users_report.sh
@@ -3,5 +3,5 @@
 #Generate a system users report by extracting username and shell from /etc/passwd
 
 while IFS= read -r line; do
-    awk -F: '{print "Username:", $1, "Shell:", $7}' <<< "$line"
+    awk -F: '{print "Username:", $1, "Shell:", $7}' <<< "$line" >> output/users_report.txt
 done < /etc/passwd
